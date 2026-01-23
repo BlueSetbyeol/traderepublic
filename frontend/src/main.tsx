9d1ed88780e5b88fcb6a82e33eee1356d7621ef2
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-01-23 16:18:19 +0100

    fix: package in frontend, npm, forth try

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
+import App from "./App.tsx";
 
 const router = createBrowserRouter([
   {
