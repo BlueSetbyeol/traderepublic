9b9e2f39109a62439fed8aeee9523f968d7faf44
BlueSetbyeol <setbyeol.c@gmail.com>
2026-01-24 10:53:33 +0100

    feat:end of navbar (#3)

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Button } from "@mui/material";
 
 export default function Navbar() {
   return (
@@ -9,18 +10,35 @@ export default function Navbar() {
         Faites fructifier votre argent de manière responsable. Découvrez tous
         les avantages et risques <ChevronRightIcon />
       </p>
-      <Link to="/">
-        <img src="/src/assets/images/trade_republic_white.png" alt="Logo" />
-      </Link>
-      <section>
-        <Link to="">Connexion</Link>
-        <button className="qrcode">
-          <p>Télécharger l'app</p>
+      <section className="navigation">
+        <Link to="/">
           <img
-            src="/src/assets/images/traderepublic_qr_code.png"
-            alt="connectez vous"
+            src="/src/assets/images/trade_republic_white.png"
+            alt="Logo"
+            className="logo"
           />
-        </button>
+        </Link>
+        <section className="connect_area">
+          <Link to="">Connexion</Link>
+          <Button
+            variant="contained"
+            className="qrcode"
+            sx={{
+              borderRadius: "18px",
+              padding: "0 .5em",
+              backgroundColor: "var(--primary-color)",
+              color: "var(--primary-font-color)",
+              fontFamily: "var(--font-family)",
+              textTransform: "none",
+            }}
+          >
+            <p>Télécharger l'app</p>
+            <img
+              src="/src/assets/images/traderepublic_qr_code.png"
+              alt="connectez vous"
+            />
+          </Button>
+        </section>
       </section>
     </nav>
   );
