32c134f2bcd317b7dfba42001b7669ff95024467
BlueSetbyeol <setbyeol.c@gmail.com>
2026-02-09 16:02:15 +0100

    feat: initializing Confiance section with a component to add into Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import "./Home.css";
 import Faq from "../../components/Faq/Faq";
+import Confiance from "../../components/Confiance/Confiance";
 
 export default function Home() {
   return (
     <>
       <section className="hero">
-        <h1>La manière la plus intelligente d'investir, de dépenser et d'épargner.</h1>
+        <h1>
+          La manière la plus intelligente d'investir, de dépenser et d'épargner.
+        </h1>
       </section>
+      <Confiance />
       <Faq />
     </>
   );
